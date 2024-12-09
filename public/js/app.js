f0fe743aff714a0d50d5738513fa07cfe14aa7b3
john petro <johnpetro335@gmail.com>
2024-12-09 13:30:07 +0300

    commit2

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,13 @@ const scroll = ScrollReveal({
   scroll.reveal('.text-transform',{delay:100,origin:'left'})
   scroll.reveal('.text-hero',{delay:200,origin:'top'})
   scroll.reveal('.hero-transform',{delay:100,origin:'right'})
+
+
+const menu =  document.querySelector('#menu');
+menu.addEventListener('click',(e)=>{
+   let list =  document.querySelector('#list');
+   if(list.classList.contains('men')){
+    list.classList.toggle('hidden')
+   }
+  e.preventDefault();
+})
